2629b0ef37326f2453ac42ebae0df858ee463e11
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-02-23 20:42:25 +0000

    first Commit

---
+++ Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdio.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_ti_config.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/linkage.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdlib.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
+main.obj: D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
+main.obj: D:/ti/tivaware_c_series_2_1_4_178/inc/hw_ints.h
+main.obj: D:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+main.obj: D:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+main.obj: D:/ti/tivaware_c_series_2_1_4_178/driverlib/ssi.h
+main.obj: D:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+main.obj: D:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+
+../main.c: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdio.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_ti_config.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/linkage.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdlib.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
+D:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
+D:/ti/tivaware_c_series_2_1_4_178/inc/hw_ints.h: 
+D:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+D:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+D:/ti/tivaware_c_series_2_1_4_178/driverlib/ssi.h: 
+D:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+D:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h: 
---
+++ main.c
@@ -0,0 +1,45 @@
+/**
+ * main.c
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/ssi.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+
+int main(void)
+{
+//    system clock setup(80Mhz);
+    SysCtlClockSet(SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ|SYSCTL_SYSDIV_2_5);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5);
+    while(!(SysCtlPeripheralReady(SYSCTL_PERIPH_SSI0)))
+          {
+
+          }
+    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 1000000, 16);
+    SSIEnable(SSI0_BASE);
+    SSIDataPut(SSI0_BASE, 0x0A0A);
+    SysCtlDelay(13333);
+    SSIDataPut(SSI0_BASE, 0x000F);
+    SSIDataPut(SSI0_BASE, 0x000B);
+    SSIDataPut(SSI0_BASE, 0x0009);
+    SSIDataPut(SSI0_BASE, 0x0100);//clear
+    SSIDataPut(SSI0_BASE, 0x010C);//shutdown address
+    while(1){
+      SSIDataPut(SSI0_BASE, 0xFF00);
+//      SysCtlDelay((SysCtlClockGet()/1000*3)*1000);
+      SysCtlDelay(133333);
+      SSIDataPut(SSI0_BASE, 0x0000);
+      SysCtlDelay(133333);
+    }
+//	return 0;
+}
